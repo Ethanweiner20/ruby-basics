1d3230323466d043c9b76cb87ae2279fd371df4b
Ethan Weiner <ethanweiner20@gmail.com>
2021-10-23 10:35:55 -0400

    Methods

---
+++ methods/10.rb
@@ -0,0 +1,19 @@
+# The Array#sample method can be used to randomly sample an array
+
+def name(names)
+  names.sample
+end
+
+def activity(activities)
+  activities.sample
+end
+
+def sentence(name, activity)
+  "#{name} went #{activity} today!"
+end
+
+
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+puts sentence(name(names), activity(activities))
